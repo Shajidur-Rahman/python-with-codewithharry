64ab50a5aedd1f4954e4ef1302580b33e1aa8750
Shajidur-Rahman <shajidurrahmansaad10@gmail.com>
2021-05-14 07:08:21 +0600

    chapter 02 started

---
+++ Chapter-2/01_main.py
@@ -0,0 +1,16 @@
+name = 'shajidur rahman'
+age = '12'
+mark = 90.23
+ishot = True
+
+# printing the veriables
+print(name)
+print(age)
+print(mark)
+print(ishot)
+
+# Printing the types of veriables
+print(type(name))
+print(type(age))
+print(type(mark))
+print(type(ishot))
---
+++ Chapter-2/02_main.py
@@ -0,0 +1,17 @@
+a = 2
+b = 3
+print( 'the value of a+b is ',a+b)
+print( 'the value of a-b is ',a-b)
+print( 'the value of a*b is ',a*b)
+print( 'the value of a/b is ',a/b)
+
+# assignment opatetor
+
+c = 12
+c -= 2
+print(c)
+
+# comparision oparator
+
+d = 5>7
+print(d)
