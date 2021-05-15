b3c7dd0bfc7a0108cc1111509df7849e64e1d8ea
Shajidur-Rahman <shajidurrahmansaad10@gmail.com>
2021-05-15 16:20:45 +0600

    chapter 04 finished

---
+++ Chapter-4/06_pr.py
@@ -0,0 +1,52 @@
+# first problem
+
+# first = input("Enter the name of the first frouts: ")
+# second = input("Enter the name of the second frouts: ")
+# third = input("Enter the name of the third frouts: ")
+# fourth = input("Enter the name of the fourth frouts: ")
+# fivet = input("Enter the name of the five frouts: ")
+# six = input("Enter the name of the six frouts: ")
+# seven = input("Enter the name of the seven frouts: ")
+
+# frouts = []
+# frouts.append(first)
+# frouts.append(second)
+# frouts.append(third)
+# frouts.append(fourth)
+# frouts.append(fivet)
+# frouts.append(six)
+# frouts.append(seven)
+# print(frouts)
+
+# num 2 problem
+
+# first = int(input('Enter mark: '))
+# second = int(input('Enter mark: '))
+# third = int(input('Enter mark: '))
+# four = int(input('Enter mark: '))
+# five = int(input('Enter mark: '))
+# six = int(input('Enter mark: '))
+
+# mark = []
+# mark.append(first)
+# mark.append(second)
+# mark.append(third)
+# mark.append(four)
+# mark.append(five)
+# mark.append(six)
+# mark.sort()
+# print(mark)
+
+
+# num 3 problem
+# t = (3,4,6,5,5,3)
+# t[3] = 45
+# print(t)
+
+# num 4 problem
+# a = [2,3,5,6]
+# print(a[0] + a[1] + a[2] + a[3])
+
+# num 5 problem
+tou = (7,0,8,0,0,9)
+print(tou.count(0))
