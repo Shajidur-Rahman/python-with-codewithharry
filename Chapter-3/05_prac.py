cd00f55c187865f2abe6175496e646d0e9274266
Shajidur-Rahman <shajidurrahmansaad10@gmail.com>
2021-05-15 11:19:19 +0600

    chapter 03 finished

---
+++ Chapter-3/05_prac.py
@@ -0,0 +1,29 @@
+# 1 num problem 
+# name = input('Enter your name ===>\n ')
+# print('Good Afternoon, '+ name)
+
+# 2 num problem
+# letter = '''Dear <|name|>,
+# your are selected
+
+# Date: <|date|>
+# '''
+# name = input('Enter your name ===>\n ')
+# date = input('Enter Date ===>\n ')
+# letter = letter.replace('<|name|>', name) 
+# letter = letter.replace('<|date|>', date)
+# print(letter)
+
+# 3 num problem
+# some = 'my name  is dhsjidf  fjsj'
+# some = some.find('  ')
+# print(some)
+
+# 4 num problem
+# some1 = 'my name  is dhsjidf  fjsj'
+# some1 = some1.replace('  ', ' ')
+# print(some1)
+
+# num 5 problem
+letter = 'Dear Harry,\n this python course is nice.\n Thanks'
+print(letter)
