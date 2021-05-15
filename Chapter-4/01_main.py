2ee93b6a2d974432290ebca0096368fb233d0cec
Shajidur-Rahman <shajidurrahmansaad10@gmail.com>
2021-05-15 15:42:41 +0600

    chapter 04 main finished

---
+++ Chapter-4/01_main.py
@@ -0,0 +1,7 @@
+a = [2,434,54,5,4,5,4         ,4,4]
+3
+#print(a)
+
+friends = ['saad', 'shaon', 'muhib', 'rohan', 'name']
+friends = friends[0:3]
+print(friends)
---
+++ Chapter-4/05_main.py
@@ -0,0 +1,3 @@
+tou = ('name', 'age', 'roll')
+tou = tou[0:2]
+print(tou)
