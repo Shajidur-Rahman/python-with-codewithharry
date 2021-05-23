b28948b751fb064eafbd5fd6fcea6a4b3ccb0079
Shajidur-Rahman <shajidurrahmansaad10@gmail.com>
2021-05-23 17:08:52 +0600

    format

--- Chapter-13-adv/01_main.py
+++ Chapter-13-adv/01_main.py
@@ -1,1 +1,7 @@
-import pygame
+def lam(a):
+    return 2 + a
+x = 3
+print(lam(x))
+
+fun = lambda a: a +2 
+print(fun(x))
---
+++ Chapter-13-adv/02_main.py
@@ -0,0 +1,4 @@
+name = 'shaidur rahman'
+age = 12
+bio = 'his name is {} and he is {}'.format(name, age)
+print(bio)
